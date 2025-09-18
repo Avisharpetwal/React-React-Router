c4d2ea05325bfe0f16d404f434b991fd25c5f1db
Avishar37 <avisharpetwal37@gmail.com>
2025-09-18 16:02:23 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./About"
 import { NavLink } from "react-router"
 import { Navbar } from "./Navbar"
 import Contact from "./Contact"
+import PageNotFound from "./PageNotFound"
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<Contact/>} />
+        <Route path='/*' element={<PageNotFound/>}/>
 
 
 
---
+++ src/PageNotFound.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound(){
+    return(
+        <div style={{textAlign:"center"}} >
+        <h1>Page Not Found</h1>
+        <h1>404 Page</h1>
+        </div>
+    )
+}
